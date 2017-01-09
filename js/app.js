e990fe3082caaf9b727be168b97f2d04e6091082
busyrat <1435858847@qq.com>
2017-01-09 13:00:12 +0800

    Revert "新建模仿微信框架"
    This reverts commit f5d6551f87adfa83892c3c7d673dde1ea2b725c6.

--- js/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-define(['angular-md5'],function(md5){
-    console.log(angular);
-    var app = angular.module('app', ['ionic']);
-    // run方法用于初始化全局的数据，仅对全局作用域起作用。
-    app.run(['$rootScope',function($rootScope){
-        console.log($rootScope);
-    }])
-})
